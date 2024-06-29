52e6c3b97d67d9179f0de99194fc27e69e9ced78
santu5992 <sayantanmandal500@gmail.com>
2024-06-30 04:26:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "60"))
+SECONDS = int(os.getenv("SECONDS", "600"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -64,6 +64,7 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
         
+        CodeXBotz = []
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.video):
@@ -78,18 +79,21 @@ async def start_command(client: Client, message: Message):
 
             try:
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(1)               
+                await asyncio.sleep(2)
+                CodeXBotz.append(snt_msg)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)                
+                snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                CodeXBotz.append(snt_msg)
             except:
                 pass
 
         h = await message.reply_text("<b>❗️❗️❗ <u>WARNING!</u> ❗️❗️❗️</b>\n\n<b>This video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.</b>")
         await asyncio.sleep(SECONDS)
-        
+
+        for data in CodeXBotz:
             try:
-                await snt_msg.delete()       
+                await data.delete()       
                 await h.edit_text("<b>Your video / file is successfully deleted !</b>")
             except:
                 pass                
