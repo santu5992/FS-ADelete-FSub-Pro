abc5427a7b07fe02eb27687e29b0e5eaf8dec4f2
santu5992 <sayantanmandal500@gmail.com>
2024-07-02 21:24:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-# SECONDS = int(os.getenv("SECONDS", "600"))
+SECONDS = int(os.getenv("SECONDS", "600"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -79,7 +79,7 @@ async def start_command(client: Client, message: Message):
 
             try:
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(0.6)
+                await asyncio.sleep(0.2) 
                 CodeXBotz.append(snt_msg)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
         h = await message.reply_text("<b>❗️❗️❗ <u>WARNING!</u> ❗️❗️❗️</b>\n\n<b>This video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.</b>")
-        await asyncio.sleep(600)
+        await asyncio.sleep(SECONDS)
 
         for data in CodeXBotz:
             try:
